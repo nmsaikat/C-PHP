b33be48fe2b086036e2f1d67cb3a79365a5aec0f
nmsaikat <nmsaikat.cf@hmail.com>
2024-10-02 18:34:46 +0600

    code update

---
+++ Intro to Variables/index.php
@@ -0,0 +1,14 @@
+<?php
+// Variable
+$name = "Bangladesh";
+$age = 16;
+$question = "How are you?";
+
+$word = "age";
+
+echo 'hello'.$name;
+echo "\n";
+
+echo "Hello $name";
+echo "\n";
+echo "Hello {$name}, {$question}";
---
+++ Variables, Constants and Comments/index.php
@@ -0,0 +1,11 @@
+<?php
+define('PI' ,3.14159);
+$task = "Read";
+echo $task;
+echo"\n";
+$task = "write";
+echo $task;
+echo "\n";
+
+// echo"Value of PI = {PI}".PI;
+echo PI;
